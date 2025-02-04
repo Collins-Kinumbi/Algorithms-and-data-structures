be199da3fdccd69990b716d9c8b84e6f42df5b1f
Collins Kinumbi <kinumbicollins254@gmail.com>
2025-02-04 12:26:27 +0300

    Comment answer fix

--- Algorithms/math/factorial.js
+++ Algorithms/math/factorial.js
@@ -10,5 +10,5 @@ function factorial(n) {
 
 console.log(factorial(0)); //1
 console.log(factorial(1)); //1
-console.log(factorial(3)); //1
+console.log(factorial(3)); //6
 console.log(factorial(5)); //120
