5e5f3861f6e0ea298c8306145f5038ce62099ee1
Collins Kinumbi <kinumbicollins254@gmail.com>
2025-02-04 11:53:24 +0300

    Recursive fibonacci

---
+++ Algorithms/math/fibonacci-recursive.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function recursiveFibonacci(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+}
+
+console.log(recursiveFibonacci(7));
+
+// Big O - O(2^n)
