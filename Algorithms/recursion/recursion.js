555646aab33a55b12dd29e0ff867b09fcf4f4d00
Collins Kinumbi <kinumbicollins254@gmail.com>
2025-02-14 17:36:53 +0300

    Solving sum numbers using recursion

--- Algorithms/recursion/recursion.js
+++ Algorithms/recursion/recursion.js
@@ -1,1 +1,42 @@
 "use strict";
+const nums1 = [1, 2, 3, 4, 5];
+const nums2 = [1, [2], 3, [4, 5]];
+
+// Without recursion
+/*
+function sumNumbers(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i]);
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        // console.log(arr[i][j]);
+        sum += arr[i][j];
+      }
+    } else {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
+
+console.log(sumNumbers(nums1));
+console.log(sumNumbers(nums2));
+*/
+
+// using recursion
+function sumNumbers(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i]);
+    if (Array.isArray(arr[i])) {
+      sum += sumNumbers(arr[i]);
+    } else {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
+
+// console.log(sumNumbers(nums1));
+// console.log(sumNumbers(nums2));
