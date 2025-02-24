58021d5b94959b62a3fee1675a3ffd33a0e507e4
Collins Kinumbi <kinumbicollins254@gmail.com>
2025-02-24 13:58:03 +0300

    Optimized queue

--- Data structures/queue.js
+++ Data structures/queue.js
@@ -2,43 +2,49 @@
 
 class Queue {
   constructor() {
-    this.items = [];
+    this.items = {};
+    this.rear = 0;
+    this.front = 0;
   }
 
   enqueue(element) {
-    this.items.push(element);
+    this.items[this.rear] = element;
+    this.rear += 1;
   }
 
   dequeue() {
-    return this.items.shift();
+    if (this.isEmpty()) return "Queue is empty";
+    const item = this.items[this.front];
+    delete this.items[this.front];
+    this.front += 1;
+    return item;
   }
 
   isEmpty() {
-    return this.items.length === 0;
+    return this.rear - this.front === 0;
   }
 
   peek() {
-    if (!this.isEmpty()) {
-      return this.items[0];
-    }
-    return null;
+    return this.items[this.front];
   }
 
   size() {
-    return this.items.length;
+    return this.rear - this.front;
   }
 
   print() {
-    console.log(this.items.toString());
+    // console.log(this.items);
+    console.log(Object.values(this.items).join(", "));
   }
 }
 
 const queue = new Queue();
 
 console.log(queue.isEmpty());
-queue.enqueue("Bruce");
+
 queue.enqueue(1);
-// queue.dequeue();
+queue.enqueue(2);
+queue.enqueue(3);
 console.log(queue.size());
 queue.print();
 console.log(queue.peek());
