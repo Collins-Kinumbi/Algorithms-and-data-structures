e9c768e6a2ba6dc3460a29fcb17c43ea40161966
Collins Kinumbi <kinumbicollins254@gmail.com>
2025-03-18 12:22:57 +0300

    Insertion

--- Data structures/binarySearchTree.js
+++ Data structures/binarySearchTree.js
@@ -15,8 +15,40 @@ class BinarySearchTree {
   isEmpty() {
     return this.root === null;
   }
+
+  insert(value) {
+    const node = new Node(value);
+
+    if (this.isEmpty()) {
+      this.root = node;
+    } else {
+      this.insertNode(this.root, node);
+    }
+  }
+
+  insertNode(root, newNode) {
+    if (newNode.value < root.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
 }
 
 const bst = new BinarySearchTree();
 
 console.log("Tree is empty:", bst.isEmpty());
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+
+console.log(bst);
