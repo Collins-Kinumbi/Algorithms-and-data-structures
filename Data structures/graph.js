855525da27f09b42a159781fa7c694a3971301e4
Collins Kinumbi <kinumbicollins254@gmail.com>
2025-03-19 12:13:18 +0300

    Graph add vertex and edge

--- Data structures/graph.js
+++ Data structures/graph.js
@@ -1,5 +1,36 @@
 "use strict";
 
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = new Set();
+    }
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1]) {
+      this.addVertex(vertex1);
+    }
+    if (!this.adjacencyList[vertex2]) {
+      this.addVertex(vertex2);
+    }
+    this.adjacencyList[vertex1].add(vertex2);
+    this.adjacencyList[vertex2].add(vertex1);
+  }
+}
+
+const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addEdge("A", "B");
+graph.addEdge("B", "C");
+console.log(graph);
+
+/* 
 // Adjacency matrix
 const matrix = [
   [0, 1, 0],
@@ -17,3 +48,4 @@ const adjacencyList = {
 };
 
 console.log(adjacencyList["B"]);
+ */
