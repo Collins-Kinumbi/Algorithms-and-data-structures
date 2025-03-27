744dbea675a5fb34416b6445d17d3c57bd495634
Collins Kinumbi <kinumbicollins254@gmail.com>
2025-03-27 17:52:29 +0300

    Same array check problem

--- Problems/problems.js
+++ Problems/problems.js
@@ -1,5 +1,36 @@
 "use strict";
 
+// Write a function called 'same' it should take two arrays and check if the second array contains the squares of the values in the first array
+
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  const frequencyCounter1 = {};
+  const frequencyCounter2 = {};
+
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// console.log(same([1, 2, 3], [4, 9, 1]));
+// console.log(same([1, 2, 3, 5], [4, 9, 1]));
+
+///////////////////////////////////////////
+
 // Write a function that returns the number of a alpha numeric character in a string
 
 function charCount(str) {
@@ -22,4 +53,4 @@ function charCount(str) {
   return hash;
 }
 
-console.log(charCount("Hello there!"));
+// console.log(charCount("Hello there!"));
