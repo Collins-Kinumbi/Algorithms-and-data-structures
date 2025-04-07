6d909d3963036ff5db97503c8c55d2692e5db5e9
Collins Kinumbi <kinumbicollins254@gmail.com>
2025-04-07 13:29:53 +0300

    BST problems

--- Data structures/binarySearchTree.js
+++ Data structures/binarySearchTree.js
@@ -122,6 +122,7 @@ class BinarySearchTree {
     }
     return visited;
   }
+
   /////////////////////////////////////////
 
   min(root) {
@@ -164,6 +165,71 @@ class BinarySearchTree {
       root.right = this.deleteNode(root.right, root.value);
     }
     return root;
+  }
+
+  /////////////////////////////////////////
+  //Problems:
+
+  /*
+  Challenge 2: Medium
+  Problem:
+  Given a binary tree, return true if it contains a target value, or false if not.
+  Implement:
+
+  One solution with DFS (any of pre/in/post order is fine)(Find method up top)
+
+  One solution with BFS
+ */
+
+  BFSFind(value) {
+    if (this.isEmpty()) return false;
+
+    const queue = [this.root];
+
+    while (queue.length) {
+      let current = queue.shift();
+
+      if (current.value === value) return true;
+
+      if (current.left) {
+        queue.push(current.left);
+      }
+      if (current.right) {
+        queue.push(current.right);
+      }
+    }
+    return false;
+  }
+
+  /*
+  Challenge 3: Medium-Hard
+  Problem:
+  Find the minimum depth of the binary tree.
+  The minimum depth is the shortest path from root to any leaf node.
+
+  Hint: BFS is very good for this because it finds the shortest path first!
+  */
+
+  minDepth() {
+    if (this.isEmpty()) return 0;
+
+    const queue = [[this.root, 1]];
+
+    while (queue.length) {
+      const [current, depth] = queue.shift();
+
+      if (!current.left && !current.right) {
+        return depth;
+      }
+
+      if (current.left) {
+        queue.push([current.left, depth + 1]);
+      }
+
+      if (current.right) {
+        queue.push([current.right, depth + 1]);
+      }
+    }
   }
 }
 
