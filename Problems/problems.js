ca48ae37109195225e5710c7fd669583df50f32b
Collins Kinumbi <kinumbicollins254@gmail.com>
2025-05-21 16:29:12 +0300

    Roman to integer

--- Problems/problems.js
+++ Problems/problems.js
@@ -1,5 +1,59 @@
 "use strict";
 
+/*
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+*/
+
+function romanToInt(string) {
+  const hash = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }; // basically put the 7 building blocks of Roman symbols in a hash map
+
+  let sum = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    let current = hash[string[i]];
+    let next = hash[string[i + 1]];
+
+    if (next > current) {
+      sum += next - current;
+      i += 1; // skip next since we already used it
+      // Remember for loop increments i by 1 after code execution within the loop finishes in each iteration
+    } else {
+      sum += current; // Just add the current value
+    }
+  }
+  return sum;
+}
+
+// console.log(romanToInt("MCMXCIV"));
+// console.log(romanToInt("LVIII"));
+
+///////////////////////////////////////////
+
 // You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string. Return the merged string.
 
 function mergeAlternately(word1, word2) {
