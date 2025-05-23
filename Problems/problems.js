4aeb63e050b0c013b75513055fd09c1bf347f3e5
Collins Kinumbi <kinumbicollins254@gmail.com>
2025-05-23 13:24:22 +0300

    Summary ranges

--- Problems/problems.js
+++ Problems/problems.js
@@ -1,6 +1,46 @@
 "use strict";
 
 /*
+You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+"a->b" if a != b
+"a" if a == b
+*/
+
+function summaryRanges(nums) {
+  let ranges = [];
+
+  let i = 0;
+  while (i < nums.length) {
+    let start = nums[i];
+
+    // Move `i` forward as long as numbers are consecutive
+    while (i + 1 < nums.length && nums[i] + 1 === nums[i + 1]) {
+      i += 1;
+    }
+
+    // If start === nums[i], it's a single number
+    if (start === nums[i]) {
+      ranges.push(start.toString());
+    } else {
+      ranges.push(`${start}->${nums[i]}`);
+    }
+    i++; // move to the next potential range
+  }
+  return ranges;
+}
+
+// console.log(summaryRanges([0, 1, 2, 4, 5, 7]));
+
+///////////////////////////////////////////
+
+/*
 Write a function to find the longest common prefix string amongst an array of strings.
 
 If there is no common prefix, return an empty string "".
