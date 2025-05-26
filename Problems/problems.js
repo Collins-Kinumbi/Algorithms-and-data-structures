9bed584516e52d88d7ba0abf78ceebe252183bd8
Collins Kinumbi <kinumbicollins254@gmail.com>
2025-05-26 15:00:24 +0300

    Merge interval

--- Problems/problems.js
+++ Problems/problems.js
@@ -1,6 +1,51 @@
 "use strict";
 
 /*
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+*/
+
+function merge(intervals) {
+  // Sort intervals based on the starting time
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  // console.log(intervals);
+
+  const merged = [];
+  for (const interval of intervals) {
+    // If the merged list is empty or there is no overlap
+    if (merged.length === 0 || merged[merged.length - 1][1] < interval[0]) {
+      merged.push(interval);
+    } else {
+      // There is overlap, merge the intervals
+      merged[merged.length - 1][1] = Math.max(
+        merged[merged.length - 1][1],
+        interval[1]
+      );
+    }
+  }
+
+  return merged;
+}
+
+console.log(
+  merge([
+    [1, 3],
+    [2, 6],
+    [8, 10],
+    [15, 18],
+  ])
+);
+
+console.log(
+  merge([
+    [1, 4],
+    [4, 5],
+  ])
+);
+
+//////////////////////////////////////////
+
+/*
 Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 
 The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
